4f477123c44ac1147e3615df6e28d57aa5854c9d
tager1199 <32684792+tager1199@users.noreply.github.com>
2019-06-19 16:50:40 +0100

    updated
    updated to the new discord.py

--- DoggoBot.py
+++ DoggoBot.py
@@ -25,7 +25,6 @@ access_token_secret =  t.read()
 consumer_key = "anEmKfK4WLIW5IIPyQk7mgWLn"
 consumer_secret = c.read()
 pic_ext = ['.jpg','.png','.jpeg']
-
 api = twitter.Api(consumer_key,consumer_secret,access_token,access_token_secret, tweet_mode='extended')
 rate = ["Excellent Woofer", "Much Cute", "Lovely Doggo", "Thats a big ol' pupper"]
 
@@ -38,13 +37,15 @@ client = discord.Client()
 async def on_message(message):
     rand = random.randint(0,200)
     text = message.content.lower()
-
+    msg = ""
+    author = message.author
+    channel = message.channel
     #Add Emoji Reaction to any message containing 'bork'
     if "bork" in text:
         await client.add_reaction(message, "\U0001F415")
 
     #if message author is this bot
-    if message.author == client.user:
+    if author == client.user:
         #Add emoji reaction to any message sent by the bot
         emoji = get(client.get_all_emojis(), name="\U0001F436")
         await client.add_reaction(message, "\U0001F436")
@@ -52,7 +53,7 @@ async def on_message(message):
         return
 
     #if author is any bot
-    if message.author.bot == True:
+    if author.bot == True:
         #stop bot from responding
         return
 
@@ -63,14 +64,21 @@ async def on_message(message):
     #if message starts with 'hello doggo', say hello and @ the person who said it
     if text.startswith('hello doggo'):
         msg = 'Henlo {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
+
+    if text.startswith('help doggo'):
+        msg = "what are you thinking doggo? - Doggo will let you know what its thinking about\ndoggo show me a doggo - Doggo will send you a picture of a doggo\
+        \ndoggo rate my doggo - if an image is attached doggo will rate it\
+        \n\n***If you have an issue with the bot please contact tinyman1199#6969***"
+
+        await message.channel.send(msg)
 
     #if message starts with 'what are you thinking doggo?'
     if text.startswith('what are you thinking doggo?'):
         #get random tweet from the dict of tweets
         msg = tweets[random.randint(0,20)]['full_text']
         #send tweet as message
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     #if message starts with 'doggo show me a doggo' send a pic of a doggo from folder
     if text.startswith('doggo show me a doggo'):
@@ -80,14 +88,14 @@ async def on_message(message):
         #get random number for image
         rando = random.randint(1,number_files)
         #send the file
-        with open(dir_path+'/Doggos/'+l[rando-1], 'rb') as picture:
-            m = await client.send_file(message.channel,picture)
-        await client.add_reaction(m, "❤")
+        path = dir_path+'/Doggos/'+l[rando-1]
+        m = await channel.send(file=discord.File(path))
+        await m.add_reaction("❤")
 
-    if "test" in text:
-        embed = discord.Embed();
-        embed.set_thumbnail(url='https://images.dog.ceo/breeds/chow/n02112137_5089.jpg')
-        await client.send_message(message.channel, embed=embed)
+    #if "test" in text:
+    #    embed = discord.Embed();
+    #    embed.set_thumbnail(url='https://images.dog.ceo/breeds/chow/n02112137_5089.jpg')
+    #    await message.channel.send(embed=embed)
 
     #if some asks 'doggo rate my doggo' and they have a file
     if text.startswith('doggo rate my doggo') and "filename" in message.attachments[0]:
@@ -95,38 +103,38 @@ async def on_message(message):
             #if attached file is an image
             if (message.attachments[0]["filename"]).endswith(i):
                 #react with a heart to the image they sent
-                await client.add_reaction(message, "❤")
+                await message.add_reaction("❤")
                 #get random numbers for rating and message responce
                 choice = random.randint(0,len(rate))
                 rating = random.randint(10,21)
                 #reply with at least 10/10 and a random message from list
                 msg = str(rating) + "/10 " + rate[choice]
-                await client.send_message(message.channel,msg)
+                await message.channel.send(msg)
 
     #if message contains 'good boy' or 'good boi'
     if "good boy" in text or "good boi" in text:
         #reply with 'im a good boy'
-        await client.send_message(message.channel, 'im a good boy')
+        await message.channel.send('im a good boy')
 
     #if message contains 'woof'
     if "woof" in text:
         #reply with 'bork' and vice versa
-        await client.send_message(message.channel, 'bork')
+        await message.channel.send('bork')
 
     if "bork" in text:
-        await client.send_message(message.channel, 'woof')
+        await message.channel.send('woof')
 
     #if message contains 'cat'
-    if "cat" in text:
+    if " cat " in text:
         #reply with 'WOOF! BORK WOOF!'
-        await client.send_message(message.channel, 'WOOF! BORK! WOOF!')
+        await message.channel.send('WOOF! BORK! WOOF!')
 
     #at random send a message of 'woof woof' or 'bork bork'
     if rand == 50:
-        await client.send_message(message.channel, 'woof woof')
+        await message.channel.send('woof woof')
 
     if rand == 20:
-        await client.send_message(message.channel, 'bork bork')
+        await message.channel.send('bork bork')
 
 
 
