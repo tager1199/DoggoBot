27c386c6e7cfe733b9be901c21b37d9fd0ed6581
tager1199 <32684792+tager1199@users.noreply.github.com>
2020-08-19 15:34:56 +0100

    Update DoggoBot.py

--- DoggoBot.py
+++ DoggoBot.py
@@ -12,14 +12,16 @@
 import discord
 import twitter
 import random
+import urllib.request
+import json
 import os
 from os import walk
 from discord.utils import get
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 #Variables that contains the user credentials to access Twitter API
-t = open("TwitterSecret.txt", "r")
-c = open("ConsumerSecret.txt", "r")
+t = open("/home/pi/Desktop/DoggoBot/TwitterSecret.txt", "r")
+c = open("/home/pi/Desktop/DoggoBot/ConsumerSecret.txt", "r")
 access_token = "3303963448-DYXKxwkKTeOZPScOBgXVGLDfrl8DR0ZQsrQvMQp"
 access_token_secret =  t.read()
 consumer_key = "anEmKfK4WLIW5IIPyQk7mgWLn"
@@ -28,7 +30,7 @@ pic_ext = ['.jpg','.png','.jpeg']
 api = twitter.Api(consumer_key,consumer_secret,access_token,access_token_secret, tweet_mode='extended')
 rate = ["Excellent Woofer", "Much Cute", "Lovely Doggo", "Thats a big ol' pupper"]
 
-f = open("TOKEN.txt", "r")
+f = open("/home/pi/Desktop/DoggoBot/TOKEN.txt", "r")
 TOKEN = f.read();
 
 client = discord.Client()
@@ -42,13 +44,12 @@ async def on_message(message):
     channel = message.channel
     #Add Emoji Reaction to any message containing 'bork'
     if "bork" in text:
-        await client.add_reaction(message, "\U0001F415")
+        await message.add_reaction("\U0001F415")
 
     #if message author is this bot
     if author == client.user:
         #Add emoji reaction to any message sent by the bot
-        emoji = get(client.get_all_emojis(), name="\U0001F436")
-        await client.add_reaction(message, "\U0001F436")
+        await message.add_reaction("\U0001F436")
         #stop the bot replying to itself
         return
 
@@ -66,7 +67,7 @@ async def on_message(message):
         msg = 'Henlo {0.author.mention}'.format(message)
         await message.channel.send(msg)
 
-    if text.startswith('help doggo'):
+    if (text.startswith('help doggo') or text.startswith('doggo help')):
         msg = "what are you thinking doggo? - Doggo will let you know what its thinking about\ndoggo show me a doggo - Doggo will send you a picture of a doggo\
         \ndoggo rate my doggo - if an image is attached doggo will rate it\
         \n\n***If you have an issue with the bot please contact tinyman1199#6969***"
@@ -82,14 +83,24 @@ async def on_message(message):
 
     #if message starts with 'doggo show me a doggo' send a pic of a doggo from folder
     if text.startswith('doggo show me a doggo'):
-        #get list of files
-        l = os.listdir(dir_path+'/Doggos')
-        number_files = len(l)
-        #get random number for image
-        rando = random.randint(1,number_files)
-        #send the file
-        path = dir_path+'/Doggos/'+l[rando-1]
-        m = await channel.send(file=discord.File(path))
+        decision = random.randint(0,100)
+        if (decision < 26):
+            #get list of files
+            l = os.listdir(dir_path+'/Doggos')
+            number_files = len(l)
+            #get random number for image
+            rando = random.randint(1,number_files)
+            #send the file
+            path = dir_path+'/Doggos/'+l[rando-1]
+            m = await channel.send(file=discord.File(path))
+        else:
+            with urllib.request.urlopen("https://dog.ceo/api/breeds/image/random") as url:
+                data = json.loads(url.read().decode())
+                url = data['message']
+            urllib.request.urlretrieve(url, dir_path + "//doggo.png")
+            path = dir_path+'//doggo.png'
+            m = await channel.send(file=discord.File(path))
+            os.remove(dir_path + "//doggo.png")
         await m.add_reaction("❤")
 
     #if "test" in text:
@@ -98,14 +109,14 @@ async def on_message(message):
     #    await message.channel.send(embed=embed)
 
     #if some asks 'doggo rate my doggo' and they have a file
-    if text.startswith('doggo rate my doggo') and "filename" in message.attachments[0]:
+    if text.startswith('doggo rate my doggo') and message.attachments[0]:
+        #if attached file is an image
         for i in pic_ext:
-            #if attached file is an image
-            if (message.attachments[0]["filename"]).endswith(i):
+            if (message.attachments[0].filename).endswith(i):
                 #react with a heart to the image they sent
                 await message.add_reaction("❤")
                 #get random numbers for rating and message responce
-                choice = random.randint(0,len(rate))
+                choice = random.randint(0,len(rate)-1)
                 rating = random.randint(10,21)
                 #reply with at least 10/10 and a random message from list
                 msg = str(rating) + "/10 " + rate[choice]
@@ -125,7 +136,7 @@ async def on_message(message):
         await message.channel.send('woof')
 
     #if message contains 'cat'
-    if " cat " in text:
+    if "cat" in text:
         #reply with 'WOOF! BORK WOOF!'
         await message.channel.send('WOOF! BORK! WOOF!')
 
